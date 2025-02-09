1df8d85bdd4f6df970b6a864863c780a3acd4c2b
Angus Taylor <angustaylo99@gmail.com>
2025-02-09 17:49:19 +0000

    finished up to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...args) {
+    newArray = []
+    console.log(args)
+    for (const value of array1) {
+        if (!args.includes(value)) {
+            newArray.push(value)
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
