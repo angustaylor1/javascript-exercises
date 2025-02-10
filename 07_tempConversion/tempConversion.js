530f92ded6bb158de071ab0d1a6847a889b714a1
Angus Taylor <angustaylo99@gmail.com>
2025-02-10 12:53:24 +0000

    completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celsius = (temp - 32) * (5/9) 
+  let rounded = Math.round(celsius * 10) / 10
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheight = (temp * (9/5)) + 32
+  let rounded = Math.round(fahrenheight * 10) / 10
+  return rounded
 };
 
 // Do not edit below this line
